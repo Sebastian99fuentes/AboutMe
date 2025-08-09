4f79914f3649794ccd1ee371b1518699031c5b94
Sebastián Fuentes <ssfuentes.99@gmail.com>
2025-08-08 20:45:50 -0500

    PagWebPorffolio

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { AboutMeComponent } from './about-me/about-me.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+      { 
+        path: '', 
+        component: AboutMeComponent, // Carga directa (NO lazy)
+     },
+];
